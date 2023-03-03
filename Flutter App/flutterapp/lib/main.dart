0048563e99234b956d7ce66f62bc00f6549ac4bc
abdullahi Hear <abdalaheart@gmail.com>
2023-03-03 03:26:05 -0800

    my rich app

---
+++ Flutter App/flutterapp/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+home:Scaffold(
+  appBar: AppBar(
+title: Text('I AM Richman'),
+  ),
+  body:Center(child:Text('Kusoo dhawaaw My first Flutter App')),
+ ),
+  ));
+}
