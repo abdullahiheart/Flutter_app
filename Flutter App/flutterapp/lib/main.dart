9bf97be1341e8f184b008fee73091684af8744d3
abdullahi Hear <abdalaheart@gmail.com>
2023-03-03 07:09:11 -0800

    I'm rich app

--- Flutter App/flutterapp/lib/main.dart
+++ Flutter App/flutterapp/lib/main.dart
@@ -2,11 +2,15 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
 home:Scaffold(
+  backgroundColor: Color(0xffae7b0d),
   appBar: AppBar(
 title: Text('I AM Richman'),
+centerTitle: true,
   ),
-  body:Center(child:Text('Kusoo dhawaaw My first Flutter App')),
+  body:Center(child:Image.asset('assets/images/diamond.png'),
+  ),
  ),
   ));
 }
